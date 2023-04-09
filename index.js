17ab54e24899167bdabcfecd676ecb192ec53cf5
jfoyarzo <foyarzo621@gmail.com>
2023-04-08 23:25:51 -0400

    Rename Index.js and add gitignore file

--- index.js
+++ index.js
@@ -1,40 +1,36 @@
-//Definición de variables para los módulos a utilizar
+require('dotenv').config()
 const yargs = require('yargs')
 const http = require('http')
 const jimp = require('jimp')
 const fs = require('fs')
 const url = require('url')
-//Definición de variable para la key necesaria para levantar el servidor
-const key = '123'
 
-//Función para levantar el servidor con 'http'
+const PORT = process.env.PORT || 3000
+const key = process.env.KEY
+
+
 let server = () => {
     http.createServer((req, res) => {
 
-        //Habilitación de ruta raiz del sitio
-        if (req.url == '/') {
-            //Se devuelve el contenido del archivo index.html
+        
+        if (req.url == '/') {            
             res.writeHead(200, { 'Content-Type': 'text/html' })
             fs.readFile('index.html', 'utf8', (err, html) => {
                 res.end(html)
             })
         }
-
-        //Habilitación de la ruta para los estilos CSS personalizados
+        
         if (req.url == '/estilos') {
             res.writeHead(200, { 'Content-Type': 'text/css' })
             fs.readFile('estilos.css', (err, css) => {
                 res.end(css)
             })
         }
-
-        //Habilitación de ruta para procesar la imagen
-        if (req.url.includes('/bn')) {
-            //Se captura la query de la url y se inicializa variable para la url de la imagen
+        
+        if (req.url.includes('/bn')) {            
             const params = url.parse(req.url, true).query
             const urlImg = params.url
-
-            //Instrucciones para la modificación de la imagen mediante el módulo Jimp y fs.
+            
             jimp.read(urlImg, (err, img) => {
                 if (!err) {
                     img
@@ -49,16 +45,15 @@ let server = () => {
                             })
                         })
                 } else {
-                    res.end('ERROR, no se pudo procesar el archivo, asegúrese que la url sea la correcta')
+                    res.end('There was an error processing your image, please make sure the image url is correct')
                 }
             })
         }
     })
-        .listen(3000, () => console.log('Escuchando el puerto 3000'))
+        .listen(PORT, () => console.log(`Server runing on port ${PORT}`))
 }
 
 
-//Inicialización de variable para el comando personalizado de yargs
 const yarg = yargs.command('server', 'Es un comando para levantar un servidor localmente',
     {
         key: {
@@ -67,7 +62,6 @@ const yarg = yargs.command('server', 'Es un comando para levantar un servidor lo
             alias: 'k',
         },
     },
-    //Función callback para verificar que se haya introducido la key correcta como argumento y, de ser así, ejecuta la función para levantar el servidor
     (args) => {
         args.key == key ?
             server() : console.log('Key incorrecta')
